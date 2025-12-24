9ca36afe939ce5924cad5e63616306fc44550e7e
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-12-24 11:51:36 -0300

    feat: example of reducer hook

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useReducer, useState } from "react";
 import type { TaskStateModel } from "../../models/TaskStateModel";
 import { initialTaskStateData } from "./initialTaskData";
 import { TaskContext } from "./TaskContext";
@@ -8,9 +8,20 @@ type TaskContextProviderProps = { children: React.ReactNode };
 export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
     const [state, setState] = useState<TaskStateModel>(initialTaskStateData);
 
+    const [numero, dispatch] = useReducer((state, action) => {
+        switch(action) {
+            case "INCREMENT": {
+                return state + 1;
+            }
+        }
+
+        return state;
+    }, 0);
+
     return (
         <TaskContext.Provider value={{ state, setState }}>
-            {children}
+            <h1>O Número é: {numero}</h1>
+            <button onClick={() => dispatch("INCREMENT")}>Incrementar</button>
         </TaskContext.Provider>
     );
 };
