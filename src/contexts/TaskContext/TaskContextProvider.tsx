b64e057473597a2232d8385588d0dcd04dfbe45f
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-12-25 11:20:40 -0300

    feat: counting seconds ready

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,12 +1,33 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { initialTaskStateData } from "./initialTaskData";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
+import { TimerWorkerSingleton } from "../../workers/TimerWorkerSingleton";
 
 type TaskContextProviderProps = { children: React.ReactNode };
 
 export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
     const [state, dispatch ] = useReducer(taskReducer, initialTaskStateData);
+
+    const worker = TimerWorkerSingleton.getInstance();
+
+    worker.onmessage((event) => {
+        const countDownSeconds = event.data;
+
+        console.log(countDownSeconds);
+
+        if (countDownSeconds <= 0) {
+            worker.terminate();
+        }
+    });
+
+    useEffect(() => {
+        if (!state.activeTask) {
+            worker.terminate();
+        }
+
+        worker.postMessage(state);
+    }, [worker, state]);
 
     return (
         <TaskContext.Provider value={{ state, dispatch }}>
--- src/workers/timerWorker.js
+++ src/workers/timerWorker.js
@@ -1,23 +1,28 @@
-const countPomodoro = event => {
-    console.log("WORKER recebeu:", event.data);
+let isRunning = false;
 
-    switch (event.data) {
-        case "FAVOR": {
-            self.postMessage("Sim, posso fazer um favor");
-            break;
-        }
-        case "FALA_OI": {
-            self.postMessage("OK: OI!");
-            break;
-        }
-        case "FECHAR": {
-            self.postMessage("Tá bom, vou fechar");
-            self.close();
-            break;
-        }
-        default:
-            self.postMessage("Não entendi");
-    }
+const countPomodoro = event => {
+    if (isRunning) return;
+
+    isRunning = true;
+
+    const state = event.data;
+
+    const { activeTask, secondsReamaining } = state;
+
+    const endDate = activeTask.startDate + secondsReamaining * 10 * 10 * 10;
+    const now = Date.now();
+    let coundDownSeconds = Math.ceil((endDate - now) / 1000);
+
+    const tick = () => {
+        self.postMessage(coundDownSeconds);
+
+        const now = Date.now();
+        coundDownSeconds = Math.floor((endDate - now) / 1000);
+
+        setTimeout(tick, 1000);
+    };
+    
+    tick();
 };
 
 self.onmessage = countPomodoro;
