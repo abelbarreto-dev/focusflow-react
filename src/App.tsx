a5d98c2703782209693f9a10d1019848230873e4
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-12-26 11:47:53 -0300

    feat: scroll page to top and main router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router";
-
-import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
-import { AboutPomodoro } from "./pages/AboutPomodoro";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import { Toastify } from "./components/Toastify";
+import { MainRouter } from "./components/MainRouter";
 
 import "./styles/theme.css";
 import "./styles/global.css";
 
 export const App = () => (
     <TaskContextProvider>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about-pomodoro" element={<AboutPomodoro />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </BrowserRouter>
+        <MainRouter />
         <Toastify />
     </TaskContextProvider>
 );
